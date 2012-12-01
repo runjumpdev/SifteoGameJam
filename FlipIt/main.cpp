7254be70e3e2f1b00937685128f03f6fccd0a566
John Meister <john@supersoul.co>
2012-12-01 18:03:03 -0500

    Working on new games

--- FlipIt/main.cpp
+++ FlipIt/main.cpp
@@ -59,6 +59,10 @@ public:
 BaseGame* CurrentGame;
 BaseGameCube* CurrentGameCube[gNumCubes];
 
+
+//Flip It game
+//-------------------------------------
+
 class FlipItcube : public BaseGameCube
 {
 public:
@@ -328,6 +332,283 @@ private:
 
 static FlipItGame flipItGame;
 
+
+//Head balance game
+//-------------------------------------
+
+class HeadBalancecube : public BaseGameCube
+{
+public:
+
+	bool moveSuccess = false;
+
+	// 0 - Left
+	// 1 - Right
+	// 2 - Up
+	// 3 - Down
+	// 4 - Flip
+	int moveToCheck;
+
+	SystemTime startTime;
+	int32_t changeTime;
+
+	CubeID cube;
+
+	bool started = false;
+
+	bool finished = false;
+
+	int place = -1;
+
+	HeadBalancecube()
+	{
+	}
+
+	bool isFinished()
+	{
+		return finished;
+	}
+
+	int getPlace()
+	{
+		return place;
+	}
+
+	void setPlace (int newPlace)
+	{
+		LOG ("Cube %d finished %d\n", (int)cube, newPlace);
+
+		place = newPlace;
+
+		switch (newPlace)
+		{
+		case 0:
+			cubeVideo[cube].bg0.image(vec(0,0), First);
+			break;
+		case 1:
+			cubeVideo[cube].bg0.image(vec(0,0), Second);
+			break;
+		case 2:
+			cubeVideo[cube].bg0.image(vec(0,0), Third);
+			break;
+		}
+	}
+
+	void update(TimeDelta timeStep)
+	{
+	        //LOG("update cube %i\n", (int)cube);
+	        //cubeVideo[cube].bg0.image(vec(0,0), FlipDown);
+	}
+
+	void setFlipDir (int flipDir)
+	{
+		moveToCheck = flipDir;
+	}
+
+	void init (CubeID initCube)
+	{
+		cube = initCube;
+		place = -1;
+	}
+
+	void stop()
+	{
+		started = false;
+	}
+
+	void start()
+	{
+	        //AudioTracker::play(Music);
+		startTime = SystemTime::now();
+
+		switch (moveToCheck)
+		{
+		case 0:
+			 cubeVideo[cube].bg0.image(vec(0,0), FlipLeft);
+			break;
+		case 1:
+			cubeVideo[cube].bg0.image(vec(0,0), FlipRight);
+			break;
+		case 2:
+			 cubeVideo[cube].bg0.image(vec(0,0),FlipUp);
+			break;
+		case 3:
+			 cubeVideo[cube].bg0.image(vec(0,0), FlipDown);
+			break;
+		case 4:
+			 cubeVideo[cube].bg0.image(vec(0,0), FlipOver);
+			break;
+		}
+
+		started = true;
+		finished = false;
+		moveSuccess = false;
+
+	}
+
+	void registerListener(CubeID cube)
+	{
+
+	}
+
+	void touched ()
+	{
+		LOG ("touched\n");
+		/*
+		if (moveSuccess == true)
+		{
+			moveSuccess = false;
+		}
+		*/
+	}
+
+	void setMode (int newMode)
+	{
+		moveToCheck = newMode;
+	}
+
+	void cubeMoved()
+	{
+		if (!started)
+		{
+			return;
+		}
+
+		SystemTime nowTime = SystemTime::now();
+
+		unsigned changeFlags = motion[cube].update();
+
+		auto tilt = motion[cube].tilt;
+
+		auto accel = cube.accel();
+
+//		LOG ("Accel.x %d Accel.y: %d Accel.z %d\n", (signed int)accel.x, (signed int)accel.y, (signed int)accel.z);
+
+		signed int move;
+
+		switch (moveToCheck)
+		{
+		case 0: // left
+			move = accel.x + 64;
+			break;
+		case 1: // right
+			move = accel.x - 64;
+			break;
+		case 2: // up
+			move = accel.y + 64;
+			break;
+		case 3:
+			move = accel.y - 64;
+			break;
+		case 4:
+			move = accel.z + 64;
+			break;
+		}
+
+		move = abs(move);
+
+		// Cube Face Up, Fip To the "Left" is -X axis
+		if (move < 3 && !moveSuccess)
+		{
+			moveSuccess = true;
+			LOG ("MOVE SUCCESS %d\n", move);
+
+			TimeDelta delta = nowTime - startTime;
+			changeTime = delta.milliseconds();
+
+			LOG ("DELTA TIME: %d\n", changeTime);
+
+			finished = true;
+		}
+
+	}
+};
+
+static HeadBalancecube headBalancecube[gNumCubes];
+
+class HeadBalanceGame : public BaseGame
+{
+	Random randomGen;
+
+	int flipDir;
+
+	bool gameOver = false;
+
+	bool started = false;
+
+	int lastFoundPlace = 0;
+
+	int finishedPieces = 0;
+
+public:
+    void init()
+    {
+    	randomGen.seed();
+
+		for (unsigned i = 0; i < gNumCubes; i++)
+		{
+			CurrentGameCube[i] = & flipItCube[i]; 
+			CurrentGameCube[i]->init(i);
+		}		
+    }
+
+    void start()
+    {
+        //AudioTracker::play(Music);
+    	int rand = randomGen.randint(0, 4);
+
+    	flipDir = rand;
+
+    	gameOver = false;
+    	started = true;
+
+    	lastFoundPlace = 0;
+    	finishedPieces = 0;
+
+                    for (unsigned i = 0; i < arraysize(flipItCube); i++)
+                    {
+                        flipItCube[i].setFlipDir(getFlipDir());
+                    }
+
+    }
+
+    bool update(TimeDelta timeStep)
+    {
+//        LOG("update game\n");
+
+        for (int i=0; i < arraysize(flipItCube); i++)
+        {
+        	if (flipItCube[i].isFinished() && (flipItCube[i].getPlace() < 0))
+        	{
+        		flipItCube[i].setPlace(lastFoundPlace);
+        		lastFoundPlace++;
+        		finishedPieces++;
+        	}
+        }
+
+        if (finishedPieces == arraysize(flipItCube))
+        {
+        	gameOver = true;
+        }
+
+        return gameOver;
+    }
+
+    int getFlipDir()
+    {
+    	return flipDir;
+    }
+
+private:
+
+};
+
+static HeadBalanceGame headBalanceGame;
+
+
+//Game framework
+//-------------------------------------
+
 static void InitCube(int CubeIndex, CubeID cube)
 {
     cubeVideo[CubeIndex].initMode(BG0_SPR_BG1);
@@ -365,7 +646,9 @@ enum GameState
 
 void main()
 {
-    GameState state = StateInit;
+    GameState state = StateResults;
+	Random randomGen;
+  	randomGen.seed();
 
     for (unsigned i = 0; i < arraysize(cubeVideo); i++)
     {
@@ -378,8 +661,6 @@ void main()
     TimeStep ts;
     float Delay;
     bool isDone;
-
-    CurrentGame = &flipItGame;
 
     while (1)
     {
@@ -434,6 +715,20 @@ void main()
                 {
 					state = StateInit;
 					LOG ("Results Done\n");
+					
+					int rand = randomGen.randint(0, 1);
+					switch (rand)
+					{
+					    case 0:
+						LOG ("Playing Flip It\n");
+						CurrentGame = &flipItGame;
+					    break;
+						
+					    case 1:
+						LOG ("Playing Shake\n");
+						CurrentGame = &flipItGame;
+						break;
+					} 
                 }
             break;
 
