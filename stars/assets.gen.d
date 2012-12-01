df8525e0be2c521511e9222c293ef17b016fbef6
Richie Hoagland <richie@supersoul.co>
2012-12-01 11:37:19 -0500

    Added balance game prototype.  The game folder is called stars.

---
+++ stars/assets.gen.d
@@ -0,0 +1,55 @@
+assets.gen.o: assets.gen.cpp \
+  C:\Sifteo\sifteo-sdk-windows-v0.9.8-203-gc80efac\/include\sifteo/asset.h \
+  C:\Sifteo\sifteo-sdk-windows-v0.9.8-203-gc80efac\/include\sifteo/asset/group.h \
+  C:\Sifteo\sifteo-sdk-windows-v0.9.8-203-gc80efac\/include\sifteo/abi.h \
+  C:\Sifteo\sifteo-sdk-windows-v0.9.8-203-gc80efac\/include\sifteo/abi/syscall.h \
+  C:\Sifteo\sifteo-sdk-windows-v0.9.8-203-gc80efac\/include\sifteo/abi/types.h \
+  C:\Sifteo\sifteo-sdk-windows-v0.9.8-203-gc80efac\/include\sifteo/abi/audio.h \
+  C:\Sifteo\sifteo-sdk-windows-v0.9.8-203-gc80efac\/include\sifteo/abi/events.h \
+  C:\Sifteo\sifteo-sdk-windows-v0.9.8-203-gc80efac\/include\sifteo/abi/vram.h \
+  C:\Sifteo\sifteo-sdk-windows-v0.9.8-203-gc80efac\/include\sifteo/abi/asset.h \
+  C:\Sifteo\sifteo-sdk-windows-v0.9.8-203-gc80efac\/include\sifteo/abi/elf.h \
+  C:\Sifteo\sifteo-sdk-windows-v0.9.8-203-gc80efac\/include\sifteo/limits.h \
+  C:\Sifteo\sifteo-sdk-windows-v0.9.8-203-gc80efac\/include\sifteo/macros.h \
+  C:\Sifteo\sifteo-sdk-windows-v0.9.8-203-gc80efac\/include\sifteo/math.h \
+  C:\Sifteo\sifteo-sdk-windows-v0.9.8-203-gc80efac\/include\sifteo/asset/loader.h \
+  C:\Sifteo\sifteo-sdk-windows-v0.9.8-203-gc80efac\/include\sifteo/array.h \
+  C:\Sifteo\sifteo-sdk-windows-v0.9.8-203-gc80efac\/include\sifteo/memory.h \
+  C:\Sifteo\sifteo-sdk-windows-v0.9.8-203-gc80efac\/include\sifteo/asset/audio.h \
+  C:\Sifteo\sifteo-sdk-windows-v0.9.8-203-gc80efac\/include\sifteo/asset/image.h
+
+C:\Sifteo\sifteo-sdk-windows-v0.9.8-203-gc80efac\/include\sifteo/asset.h:
+
+C:\Sifteo\sifteo-sdk-windows-v0.9.8-203-gc80efac\/include\sifteo/asset/group.h:
+
+C:\Sifteo\sifteo-sdk-windows-v0.9.8-203-gc80efac\/include\sifteo/abi.h:
+
+C:\Sifteo\sifteo-sdk-windows-v0.9.8-203-gc80efac\/include\sifteo/abi/syscall.h:
+
+C:\Sifteo\sifteo-sdk-windows-v0.9.8-203-gc80efac\/include\sifteo/abi/types.h:
+
+C:\Sifteo\sifteo-sdk-windows-v0.9.8-203-gc80efac\/include\sifteo/abi/audio.h:
+
+C:\Sifteo\sifteo-sdk-windows-v0.9.8-203-gc80efac\/include\sifteo/abi/events.h:
+
+C:\Sifteo\sifteo-sdk-windows-v0.9.8-203-gc80efac\/include\sifteo/abi/vram.h:
+
+C:\Sifteo\sifteo-sdk-windows-v0.9.8-203-gc80efac\/include\sifteo/abi/asset.h:
+
+C:\Sifteo\sifteo-sdk-windows-v0.9.8-203-gc80efac\/include\sifteo/abi/elf.h:
+
+C:\Sifteo\sifteo-sdk-windows-v0.9.8-203-gc80efac\/include\sifteo/limits.h:
+
+C:\Sifteo\sifteo-sdk-windows-v0.9.8-203-gc80efac\/include\sifteo/macros.h:
+
+C:\Sifteo\sifteo-sdk-windows-v0.9.8-203-gc80efac\/include\sifteo/math.h:
+
+C:\Sifteo\sifteo-sdk-windows-v0.9.8-203-gc80efac\/include\sifteo/asset/loader.h:
+
+C:\Sifteo\sifteo-sdk-windows-v0.9.8-203-gc80efac\/include\sifteo/array.h:
+
+C:\Sifteo\sifteo-sdk-windows-v0.9.8-203-gc80efac\/include\sifteo/memory.h:
+
+C:\Sifteo\sifteo-sdk-windows-v0.9.8-203-gc80efac\/include\sifteo/asset/audio.h:
+
+C:\Sifteo\sifteo-sdk-windows-v0.9.8-203-gc80efac\/include\sifteo/asset/image.h:
