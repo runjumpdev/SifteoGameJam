4776d8b191860555b58b780391ffad85655b18a8
Ben Gold <bengold@gmail.com>
2012-11-30 23:55:55 -0500

    HelloWorld Project

---
+++ helloWorld/main.d
@@ -0,0 +1,113 @@
+main.o: main.cpp C:\projects\sifteosdk\/include\sifteo.h \
+  C:\projects\sifteosdk\/include\sifteo/abi.h \
+  C:\projects\sifteosdk\/include\sifteo/abi/syscall.h \
+  C:\projects\sifteosdk\/include\sifteo/abi/types.h \
+  C:\projects\sifteosdk\/include\sifteo/abi/audio.h \
+  C:\projects\sifteosdk\/include\sifteo/abi/events.h \
+  C:\projects\sifteosdk\/include\sifteo/abi/vram.h \
+  C:\projects\sifteosdk\/include\sifteo/abi/asset.h \
+  C:\projects\sifteosdk\/include\sifteo/abi/elf.h \
+  C:\projects\sifteosdk\/include\sifteo/array.h \
+  C:\projects\sifteosdk\/include\sifteo/memory.h \
+  C:\projects\sifteosdk\/include\sifteo/macros.h \
+  C:\projects\sifteosdk\/include\sifteo/asset.h \
+  C:\projects\sifteosdk\/include\sifteo/asset/group.h \
+  C:\projects\sifteosdk\/include\sifteo/limits.h \
+  C:\projects\sifteosdk\/include\sifteo/math.h \
+  C:\projects\sifteosdk\/include\sifteo/asset/loader.h \
+  C:\projects\sifteosdk\/include\sifteo/asset/audio.h \
+  C:\projects\sifteosdk\/include\sifteo/asset/image.h \
+  C:\projects\sifteosdk\/include\sifteo/audio.h \
+  C:\projects\sifteosdk\/include\sifteo/cube.h \
+  C:\projects\sifteosdk\/include\sifteo/event.h \
+  C:\projects\sifteosdk\/include\sifteo/filesystem.h \
+  C:\projects\sifteosdk\/include\sifteo/metadata.h \
+  C:\projects\sifteosdk\/include\sifteo/motion.h \
+  C:\projects\sifteosdk\/include\sifteo/string.h \
+  C:\projects\sifteosdk\/include\sifteo/system.h \
+  C:\projects\sifteosdk\/include\sifteo/time.h \
+  C:\projects\sifteosdk\/include\sifteo/video.h \
+  C:\projects\sifteosdk\/include\sifteo/video/color.h \
+  C:\projects\sifteosdk\/include\sifteo/video/sprite.h \
+  C:\projects\sifteosdk\/include\sifteo/video/framebuffer.h \
+  C:\projects\sifteosdk\/include\sifteo/video/bg0rom.h \
+  C:\projects\sifteosdk\/include\sifteo/video/bg0.h \
+  C:\projects\sifteosdk\/include\sifteo/video/bg1.h \
+  C:\projects\sifteosdk\/include\sifteo/video/bg2.h \
+  C:\projects\sifteosdk\/include\sifteo/video/tilebuffer.h assets.gen.h
+
+C:\projects\sifteosdk\/include\sifteo.h:
+
+C:\projects\sifteosdk\/include\sifteo/abi.h:
+
+C:\projects\sifteosdk\/include\sifteo/abi/syscall.h:
+
+C:\projects\sifteosdk\/include\sifteo/abi/types.h:
+
+C:\projects\sifteosdk\/include\sifteo/abi/audio.h:
+
+C:\projects\sifteosdk\/include\sifteo/abi/events.h:
+
+C:\projects\sifteosdk\/include\sifteo/abi/vram.h:
+
+C:\projects\sifteosdk\/include\sifteo/abi/asset.h:
+
+C:\projects\sifteosdk\/include\sifteo/abi/elf.h:
+
+C:\projects\sifteosdk\/include\sifteo/array.h:
+
+C:\projects\sifteosdk\/include\sifteo/memory.h:
+
+C:\projects\sifteosdk\/include\sifteo/macros.h:
+
+C:\projects\sifteosdk\/include\sifteo/asset.h:
+
+C:\projects\sifteosdk\/include\sifteo/asset/group.h:
+
+C:\projects\sifteosdk\/include\sifteo/limits.h:
+
+C:\projects\sifteosdk\/include\sifteo/math.h:
+
+C:\projects\sifteosdk\/include\sifteo/asset/loader.h:
+
+C:\projects\sifteosdk\/include\sifteo/asset/audio.h:
+
+C:\projects\sifteosdk\/include\sifteo/asset/image.h:
+
+C:\projects\sifteosdk\/include\sifteo/audio.h:
+
+C:\projects\sifteosdk\/include\sifteo/cube.h:
+
+C:\projects\sifteosdk\/include\sifteo/event.h:
+
+C:\projects\sifteosdk\/include\sifteo/filesystem.h:
+
+C:\projects\sifteosdk\/include\sifteo/metadata.h:
+
+C:\projects\sifteosdk\/include\sifteo/motion.h:
+
+C:\projects\sifteosdk\/include\sifteo/string.h:
+
+C:\projects\sifteosdk\/include\sifteo/system.h:
+
+C:\projects\sifteosdk\/include\sifteo/time.h:
+
+C:\projects\sifteosdk\/include\sifteo/video.h:
+
+C:\projects\sifteosdk\/include\sifteo/video/color.h:
+
+C:\projects\sifteosdk\/include\sifteo/video/sprite.h:
+
+C:\projects\sifteosdk\/include\sifteo/video/framebuffer.h:
+
+C:\projects\sifteosdk\/include\sifteo/video/bg0rom.h:
+
+C:\projects\sifteosdk\/include\sifteo/video/bg0.h:
+
+C:\projects\sifteosdk\/include\sifteo/video/bg1.h:
+
+C:\projects\sifteosdk\/include\sifteo/video/bg2.h:
+
+C:\projects\sifteosdk\/include\sifteo/video/tilebuffer.h:
+
+assets.gen.h:
