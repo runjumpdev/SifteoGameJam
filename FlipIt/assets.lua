be911deecb7d21c0e1b5581c5d4c100579c141b0
Ben Gold <bengold@gmail.com>
2012-12-01 12:58:50 -0500

    Changes

--- FlipIt/assets.lua
+++ FlipIt/assets.lua
@@ -1,8 +1,13 @@
 IconAssets = group{ quality=9.95 }
 Icon = image{ "assets/icon.png" }
 
+FontAssets = group {}
+Font = image{"assets/font-8x16.png", pinned=true, width=8, height=16}
+
 BootstrapAssets = group{ quality=9.9 }
 FlipUp = image {"assets/flip_up.png", pinned=true}
 FlipDown = image {"assets/flip_down.png", pinned=true}
 FlipLeft = image {"assets/flip_left.png", pinned=true}
 FlipRight = image {"assets/flip_right.png", pinned=true}
+FlipOver = image {"assets/flip_over.png", pinned=true}
+Green = image {"assets/green.png", pinned=true}
--- FlipIt/main.cpp
+++ FlipIt/main.cpp
@@ -18,6 +18,8 @@
  */
 
 #include <sifteo.h>
+#include <sifteo/math.h>
+
 #include "assets.gen.h"
 using namespace Sifteo;
 
@@ -31,11 +33,149 @@ static VideoBuffer vid;
 
 static AssetSlot MainSlot = AssetSlot::allocate().bootstrap(BootstrapAssets);
 
+static TiltShakeRecognizer motion;
+
+class CubeTracker
+{
+public:
+
+	bool moveSuccess = false;
+
+	// 0 - Left
+	// 1 - Right
+	// 2 - Up
+	// 3 - Down
+	// 4 - Flip
+	int moveToCheck;
+
+	Random randomGen;
+
+	SystemTime startTime;
+	int32_t changeTime;
+
+	CubeTracker()
+	{
+		randomGen.seed();
+	}
+
+	void init ()
+	{
+		int rand = randomGen.randint(0, 4);
+
+		LOG ("Rand: %i\n", rand);
+
+		switch (rand)
+		{
+		case 0:
+			vid.bg0.image(vec(0,0), FlipLeft);
+			break;
+		case 1:
+			vid.bg0.image(vec(0,0), FlipRight);
+			break;
+		case 2:
+			vid.bg0.image(vec(0,0),FlipUp);
+			break;
+		case 3:
+			vid.bg0.image(vec(0,0), FlipDown);
+			break;
+		case 4:
+			vid.bg0.image(vec(0,0), FlipOver);
+			break;
+		}
+
+		moveToCheck = rand;
+
+		startTime = SystemTime::now();
+	}
+
+	void registerListener(CubeID cube)
+	{
+		Events::cubeAccelChange.set(&CubeTracker::cubeMoved, this);
+		Events::cubeTouch.set(&CubeTracker::touched, this);
+	}
+
+	void touched (unsigned id)
+	{
+		LOG ("touched\n");
+		CubeID cube(0);
+		if (moveSuccess == true)
+		{
+			moveSuccess = false;
+			init();
+		}
+	}
+
+	void setMode (int newMode)
+	{
+		moveToCheck = newMode;
+	}
+
+	void cubeMoved(unsigned id)
+	{
+		LOG ("Cube Moved\n");
+
+		SystemTime nowTime = SystemTime::now();
+
+		CubeID cube(0);
+
+		unsigned changeFlags = motion.update();
+
+		auto tilt = motion.tilt;
+
+		auto accel = cube.accel();
+
+		LOG ("Accel.x %d Accel.y: %d Accel.z %d\n", (signed int)accel.x, (signed int)accel.y, (signed int)accel.z);
+
+		signed int move;
+
+		switch (moveToCheck)
+		{
+		case 0: // left
+			move = accel.x + 64;
+			break;
+		case 1: // right
+			move = accel.x - 64;
+			break;
+		case 2: // up
+			move = accel.y + 64;
+			break;
+		case 3:
+			move = accel.y - 64;
+			break;
+		case 4:
+			move = accel.z + 64;
+			break;
+		}
+
+		move = abs(move);
+
+		// Cube Face Up, Fip To the "Left" is -X axis
+		if (move < 3 && moveSuccess == false)
+		{
+			moveSuccess = true;
+			LOG ("MOVE SUCCESS %d\n", move);
+			vid.bg0.image(vec(0,0), Green);
+
+			TimeDelta delta = nowTime - startTime;
+			changeTime = delta.milliseconds();
+
+			LOG ("DELTA TIME: %d\n", changeTime);
+		}
+
+	}
+};
+
 void main()
 {
-	vid.initMode(BG0);
+	vid.initMode(BG0_SPR_BG1);
 	vid.attach(0);
-	vid.bg0.image(vec(0,0), FlipLeft);
+
+	static CubeTracker tracker;
+	tracker.registerListener(0);
+
+	tracker.init();
+
+	motion.attach(0);
 
 	while (1)
 	{
